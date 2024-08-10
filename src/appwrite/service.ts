089ba038e56c89b3dede8b86622c3f589d69feea
samandeep007 <115439036+samandeep007@users.noreply.github.com>
2024-08-09 20:22:43 -0600

    chore: Update App import path and add Appwrite configuration to .env file

---
+++ src/appwrite/service.ts
@@ -0,0 +1,9 @@
+import { ID, Account, Client } from 'appwrite'
+import Config from 'react-native-config'
+import Snackbar from 'react-native-snackbar'
+
+const appwriteClient = new Client();
+
+const APPWRITE_ENDPOINT: string = Config.APPWRITE_ENDPOINT!;
+const APPWRITE_PROJECT_ID: string = Config.APPWRITE_PROJECT_ID!;
+
